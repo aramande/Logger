075be6ee2b2db0ec5111c825d41c1b15c063ce25
Andreas Johansson <andreas.johansson@expektra.se>
2014-04-11 13:30:19 +0200

    Cleaning the 'using' declarations

--- log.cs
+++ log.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections;
+﻿
+using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace Lib1
